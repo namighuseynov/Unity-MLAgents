36d5eb53a2ec73922ba4a60d5937fb76e017024a
Namik Guseinoff <94350345+namikguseinoff@users.noreply.github.com>
2023-09-07 11:41:59 +0400

    Modified TankAgent script, added tank control mechanics

--- Unity-MLAgents/Assets/Scripts/TankAgent/TankAgent.cs
+++ Unity-MLAgents/Assets/Scripts/TankAgent/TankAgent.cs
@@ -5,48 +5,73 @@ using Unity.MLAgents.Actuators;
 
 public class TankAgent : Agent
 {
-    public WheelCollider[] leftWheels;
-    public WheelCollider[] rightWheels;
+    public float motorTorque = 100f;
+    public float steerAngle = 30f;
+    public float brakeTorque = 300f;
+
+    public WheelCollider[] Wheels;
+
+    private Rigidbody rbody;
+    [SerializeField] private Transform targetPos;
 
     private void Start()
     {
-        foreach (var wheel in leftWheels)
-        {
-            wheel.steerAngle = 0;
-        }
-        foreach (var wheel in rightWheels)
-        { wheel.steerAngle = 0; }
+        rbody = GetComponent<Rigidbody>();
     }
-
     private void FixedUpdate()
     {
-        foreach (var wheel in leftWheels)
-        {
-            wheel.motorTorque = 1000;
-        }
-        foreach (var wheel in rightWheels)
-        {
-            wheel.motorTorque = 1000;
-        }
+        foreach (var wheel in Wheels) { wheel.motorTorque = motorTorque; }
     }
 
     public override void OnEpisodeBegin()
     {
-        
+        if (transform.position.y < 0)
+        {
+            Vector3 spawnPosition = new Vector3(0f, 0.5f, 0f);
+            transform.position = spawnPosition;
+        }
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetPos.position);
+
+        sensor.AddObservation(rbody.velocity.x);
+        sensor.AddObservation(rbody.velocity.z);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        
+        Vector3 signal = Vector3.zero;
+        signal.x = actions.ContinuousActions[0];
+        signal.y = actions.ContinuousActions[1];
+
+        float motorForce = motorTorque * signal.y;
+        float steeringAngle = steerAngle * signal.x;
+
+        foreach (var wheel in Wheels)
+        {
+            wheel.motorTorque = motorForce;
+        }
+
+        float distance = Vector3.Distance(transform.position, targetPos.position);
+        if (distance < 1.4f)
+        {
+            SetReward(10f);
+            EndEpisode();
+        }
+        if (transform.position.y < 0)
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        
+        var continuiousActionsOut = actionsOut.ContinuousActions;
+        continuiousActionsOut[0] = Input.GetAxisRaw("Horizontal");
+        continuiousActionsOut[1] = Input.GetAxisRaw("Vertical");
     }
 }
